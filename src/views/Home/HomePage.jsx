304ac12530e0312fe2c5d68f27045692b2c9c3e4
khris-xp <khrisbharmmano@gmail.com>
2024-02-22 13:19:10 +0700

    feat: get all menu item

--- src/views/Home/HomePage.jsx
+++ src/views/Home/HomePage.jsx
@@ -1,3 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './HomePage.css';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  const [menus, setMenus] = useState([]);
+
+  const fetcProduct = async () => {
+    const response = await axios.get(
+      'https://bubble-tea-cafe-api-production.up.railway.app/api/menu'
+    );
+    const menus_data = response.data;
+    setMenus(menus_data.data);
+  };
+
+  useEffect(() => {
+    fetcProduct();
+  }, []);
+  return (
+    <div className='grid'>
+      {menus.map((menu) => (
+        <article key={menu.Id}>
+          <img src={menu.image} alt='Sample photo' />
+          <div className='text'>
+            <h3>{menu.name}</h3>
+            <p>
+              {menu.category}, {menu.price} Bath
+            </p>
+            <button>Add Product to Cart</button>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 }
